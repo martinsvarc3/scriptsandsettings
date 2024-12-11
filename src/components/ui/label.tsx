0ec3e79fdfcf36cfc60dedd12866dce100b85b0c
martinsvarc <77626343+martinsvarc@users.noreply.github.com>
2024-12-11 20:24:53 +0100

    Update label.tsx

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -4,19 +4,20 @@ import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
 import { cn } from "@/lib/utils"
 
-const Label = React.forwardRef
-  HTMLLabelElement,
-  React.ComponentProps<typeof LabelPrimitive.Root>
->(({ className, ...props }, ref) => (
-  <LabelPrimitive.Root
-    ref={ref}
-    className={cn(
-      "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
-      className
-    )}
-    {...props}
-  />
-))
+interface LabelProps extends React.ComponentProps<typeof LabelPrimitive.Root> {}
+
+const Label = React.forwardRef<HTMLLabelElement, LabelProps>(
+  ({ className, ...props }, ref) => (
+    <LabelPrimitive.Root
+      ref={ref}
+      className={cn(
+        "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+        className
+      )}
+      {...props}
+    />
+  )
+)
 
 Label.displayName = "Label"
 
