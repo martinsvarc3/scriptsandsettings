697d5f5b668debcf3fc6b9c9dcc256388294f03a
martinsvarc <77626343+martinsvarc@users.noreply.github.com>
2024-12-13 23:48:53 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,8 +45,8 @@ export default function Home() {
   }
 
   return (
-    <main className="min-h bg-[#f2f3f8] flex items-center justify-center py-2">
-      <div className="w-full max-w-7xl flex flex-col lg:flex-row gap-3 justify-center items-stretch px-3">
+    <main className="min-h-screen bg-[#f2f3f8] flex items-center justify-center p-3">
+      <div className="w-full max-w-7xl flex flex-col lg:flex-row gap-6 justify-center items-stretch p-3">
         <div className="w-full lg:w-1/2 flex justify-center">
           <ScriptUploader />
         </div>
