3aaeb42872a1fe40bf9477f41e718b3e7a97ad21
martinsvarc <77626343+martinsvarc@users.noreply.github.com>
2024-12-15 20:11:49 +0100

    Update switch.tsx

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -3,23 +3,26 @@
 import * as React from "react"
 import * as SwitchPrimitives from "@radix-ui/react-switch"
 
-interface SwitchProps extends React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root> {
+interface SwitchProps {
+  checked?: boolean;
+  onCheckedChange?: (checked: boolean) => void;
+  id?: string;
   className?: string;
 }
 
-const Switch = React.forwardRef
-  React.ElementRef<typeof SwitchPrimitives.Root>, 
-  SwitchProps
->(({ className, ...props }, ref) => {
-  return (
+const Switch = React.forwardRef<HTMLButtonElement, SwitchProps>(
+  ({ checked, onCheckedChange, className, ...props }, ref) => (
     <SwitchPrimitives.Root
+      className={className}
+      checked={checked}
+      onCheckedChange={onCheckedChange}
       ref={ref}
       {...props}
     >
-      <SwitchPrimitives.Thumb />
+      <SwitchPrimitives.Thumb className="block h-4 w-4 rounded-full bg-white shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0" />
     </SwitchPrimitives.Root>
-  );
-});
+  )
+);
 
 Switch.displayName = "Switch"
 
